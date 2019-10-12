da775f6ff90fd09360fa5e191ef35c788345ec20
Vaala <brice.bitot@neuf.fr>
2019-10-12 22:56:13 +0200

    Affichage Article

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:newsy/tabs/first.dart';
-import 'package:newsy/tabs/second.dart';
-import 'package:newsy/tabs/third.dart';
+
+import 'package:newsy/tabs/Fav.dart';
+import 'package:newsy/tabs/Profil.dart';
+import 'package:newsy/tabs/Filter.dart';
+import 'package:newsy/tabs/News.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyHome(),
-    // Set the theme's primary color, accent color,
     theme: ThemeData(
       primarySwatch: Colors.red,
       accentColor: Colors.redAccent,
-      // Set background color
-      backgroundColor: Colors.black12,
     ),
   ));
 }
@@ -22,22 +21,17 @@ class MyHome extends StatefulWidget {
   MyHomeState createState() => MyHomeState();
 }
 
-// SingleTickerProviderStateMixin is used for animation
 class MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
-  // Create a tab controller
   TabController controller;
 
   @override
   void initState() {
     super.initState();
-
-    // Initialize the Tab Controller
-    controller = TabController(length: 3, vsync: this);
+    controller = TabController(length: 4, vsync: this);
   }
 
   @override
   void dispose() {
-    // Dispose of the Tab Controller
     controller.dispose();
     super.dispose();
   }
@@ -45,38 +39,36 @@ class MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Appbar
-      appBar: AppBar(
-        // Title
-        title: Text("Using Bottom Navigation Bar"),
-        // Set the background color of the App Bar
-      ),
-      // Set the TabBar view as the body of the Scaffold
       body: TabBarView(
-        // Add tabs as widgets
-        children: <Widget>[FirstTab(), SecondTab(), ThirdTab()],
-        // set the controller
+        children: <Widget>[News(), Fav(), Filter(), Profil()],
         controller: controller,
       ),
-      // Set the bottom navigation bar
       bottomNavigationBar: Material(
-        // set the color of the bottom navigation bar
-        // set the tab bar as the child of bottom navigation bar
         child: TabBar(
-          tabs: <Tab>[
-            Tab(
-              // set icon to the tab
-              icon: Icon(Icons.library_books),
-            ),
-            Tab(
-              icon: Icon(Icons.search),
-            ),
-            Tab(
-              icon: Icon(Icons.person),
-            ),
-          ],
-          // setup the controller
-          controller: controller,
+            tabs: <Tab>[
+              Tab(
+                  icon: Icon(
+                    Icons.library_books,
+                  )),
+              Tab(
+                icon: Icon(
+                  Icons.search,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.add,
+                ),
+              ),
+              Tab(
+                icon: Icon(
+                  Icons.person,
+                ),
+              ),
+            ],
+            controller: controller,
+            labelColor: Colors.red,
+            unselectedLabelColor: Colors.black
         ),
       ),
     );
---
+++ lib/tabs/fav.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Fav extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+          children: <Widget>[],
+        ),
+    );
+  }
+}
