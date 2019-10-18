8983eb50873a0852e3d4031e67684256ec50738f
Brice <brice.bitot@linkt.fr>
2019-10-18 14:03:53 +0200

    Animation

--- lib/components/Article.dart
+++ lib/components/Article.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'dart:developer';
+import 'dart:math';
 
-class Article extends StatelessWidget {
+class Article extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<Article> with SingleTickerProviderStateMixin{
+
   bool selected = false;
+  AnimationController animationController;
+
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = new AnimationController(
+      vsync: this,
+      duration: new Duration(seconds: 7),
+    );
+
+    animationController.repeat();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -94,13 +115,17 @@ class Article extends StatelessWidget {
                   child: FlatButton(
                     child: Row(
                       children: <Widget>[
-                        Icon(Icons.keyboard_arrow_right, color: Colors.black),
+                        Transform.rotate(angle: this.selected ? pi/2 : 0, child: Icon(Icons.keyboard_arrow_right, color: Colors.black)),
                         Text('En Savoir Plus',
                             style: TextStyle(color: Colors.black, fontSize: 16))
                       ],
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        selected = !selected;
+                      });
+                    },
                     splashColor: Colors.redAccent,
                   ),
                   height: 50,
@@ -123,7 +148,8 @@ class Article extends StatelessWidget {
                 )),
               ],
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            )
+            ),
+            Text("Intro : Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."),
           ],
         ),
         margin: const EdgeInsets.all(10.0),
--- lib/tabs/News.dart
+++ lib/tabs/News.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:newsy/components/Article.dart';
 
@@ -5,13 +7,9 @@ class News extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
-//          Article(),
-          Container(),
-        ],
-      ),
-
+      body: ListView(children: <Widget>[
+        Container(child: Article(), height: 900),
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
